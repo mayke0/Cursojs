4af19693102b817db1339c8ef24d1a45b09ac2d6
mayke0 <mayke2_0@yahoo.com>
2023-08-22 18:55:27 -0300

    new file:   aula13/index.html 	new file:   aula14/index.js 	new file:   aula15/index.js 	new file:   aula15/js/index.html 	new file:   aula15/js/script.js

---
+++ aula16/index.js
@@ -0,0 +1,37 @@
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 123
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+/*
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos.push('Luiza');
+alunos.push('Eduardo');
+
+console.log(alunos.slice(0, -2);)
+
+delete alunos[1];
+console.log(alunos[1]);
+
+const removido = alunos.shift();
+console.log(removido);
+console.log(alunos);
+
+alunos.unshift('Fábio'); // adiciona no começo 
+alunos.unshift('Luiza');
+
+alunos.push('Luiza'); // adiciona no fim
+alunos.push('Fábio');
+
+alunos[alunos.length] = 'Luiza'; // adiciona no fim
+alunos[alunos.length] = 'Fábio'; 
+alunos[alunos.length] = 'Luana';
+
+alunos[0] = 'Eduardo'; //altera
+alunos[3] = 'Luiza'; // adicionando
+
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[2]);
+*/

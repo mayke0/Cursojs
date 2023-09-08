485d0edf960b56681ee41e2bef4ba5a333a5682d
mayke0 <mayke2_0@yahoo.com>
2023-09-08 16:26:47 -0300

    atualizacao

---
+++ aula31/index.js
@@ -0,0 +1,24 @@
+// let tem escopo bloco {... bloco}
+// var SÓ  tem escopo de função
+
+// let nome = "Luiz"; //criando
+// var nome2 = "Luiz"; //criando
+
+// if (verdadeiro) {
+//   let nome = "Otávio"; //criando
+//   var nome2 = "Rogério"; // redeclarando
+
+//   if (verdadeiro) {
+//     var nome2 = "Ronaldo";
+//     let nome = "Outra coisa";
+//   }
+// }
+
+// console.log(nome, nome2);
+
+var sobrenome = 'miranda'
+
+function falaoi(){
+    console.log(sobrenome);
+}
+falaoi()
